22fe197b2a2176023fe391e7279c2d83ce684f7a
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2022-09-17 10:54:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
-from flask import render_template
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
 
-app = Flask(__name__)
+from flask_minify  import Minify
 
+from apps import app
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+DEBUG = app.config['DEBUG'] 
+
+if not DEBUG:
+    Minify(app=app, html=True, js=False, cssless=False)
+
+app.logger.info('DEBUG            = ' + str( DEBUG )                 )
+app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
+app.logger.info('ASSETS_ROOT      = ' + app.config['ASSETS_ROOT']    )
+
+if __name__ == "__main__":
+    app.run()
